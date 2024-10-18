963f85b4e9cd3d4cd671026917d14e0e6181f0c4
diannanggraini <diananggrainiaja05@gmail.com>
2024-10-18 11:22:32 +0700

    Membuat edit dan hapus guru

--- resources/views/admin/guru.blade.php
+++ resources/views/admin/guru.blade.php
@@ -8,10 +8,11 @@
     <div class="col-12">
         <div class="bg-light rounded h-100 p-4">
             @if (session('success'))
-            <div class="alert alert-succes">
-                {{ session('success') }}
-            </div>
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
             @endif
+
             <h6 class="mb-4">Data Guru</h6>
             <div class="table-responsive">
                 <a href="{{ route('admin.guru.create') }}"class="btn btn-primary btn-sm">Tambah</a>
@@ -34,11 +35,11 @@
                             <td>{{ $guru->email}}</td>
                             <td>{{ $guru->nama_guru }}</td>
                             <td>
-                                <img src="{{ asset('storage/' . $guru->foto) }} alt="" height="30">
+                                <img src="{{ asset('storage/'. $guru->foto) }}" alt="" height="30">
                             </td>
                             <td>
-                                <a href="" class="btn btn-warning btn-sm">Edit</a>
-                                <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                                <a href="{{ route('admin.guru.edit', $guru->id_guru) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <a href="{{  route('admin.guru.delete', $guru->id_guru) }}" onclick="return confirm('Yakin ingin hapus data?')" class="btn btn-danger btn-sm">Hapus</a>
                             </td>
                         </tr>
                         @endforeach
